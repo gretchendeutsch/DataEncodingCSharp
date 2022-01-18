e51b861bc9760e0a565553ea8829d28dbdaf0016
gretchendeutsch <gdeutsch2023@chadwickschool.org>
2022-01-18 10:21:46 -0800

    chore: Finish decrypting messages.

--- Cipher.cs
+++ Cipher.cs
@@ -19,8 +19,12 @@ public class Cipher
     /// <returns>The decrypted message</returns>
     public string Decrypt(string message)
     {
-        // TODO: Finish the Decrypt method
-        return null;
+        string newMessage = string.Empty;
+        foreach (char c in message)
+        {
+            newMessage +=(char)(c - this.shift);
+        }
+        return newMessage;
     }
 
     /// <summary>
@@ -37,7 +41,7 @@ public class Cipher
             //      Instead, you should to encrypt each character using the shift
             newMessage += (char)(c + this.shift); 
 
-
+        
         }
         return newMessage;
     }
--- Program.cs
+++ Program.cs
@@ -6,18 +6,44 @@ namespace DataEncodingCSharp
     {
         static void Main(string[] args)
         {
-            Cipher cipher;
-            cipher = new Cipher(5);
+            string filePath = args[0];
+            Console.WriteLine($"Loading '{filePath}'.");
+
+            string message;
+            message = System.IO.File.ReadAllText(filePath);
+            Console.WriteLine($"The encrypted message is: {message}");
 
 
-            string encrypted;
-            encrypted = cipher.Encrypt("rosebud");
-            Console.WriteLine($"The encrypted message is: '{encrypted}'");
 
-            string decrypted;
-            decrypted = cipher.Decrypt(encrypted);
-            Console.WriteLine($"The decrypted message is: '{decrypted}'");
-            
+            int shift = 1;
+
+            while (shift <= 10)
+            {
+                Cipher cipher;
+                cipher = new Cipher(shift);
+                string decrypt;
+                decrypt = cipher.Decrypt(message);
+                Console.WriteLine($"The decrypted message is: '{decrypt}'");
+                shift = shift + 1;
+            }
+
+
+
+
+            // string message;
+            // message = System.IO.File.ReadAllText("secrets/1.txt");
+            // Console.WriteLine($"The encrypted message is: '{message}");
+
+
+
+            // string encrypted;
+            // encrypted = cipher.Encrypt("rosebud");
+            // Console.WriteLine($"The encrypted message is: '{encrypted}'");
+
+            // string decrypted;
+            // decrypted = cipher.Decrypt(message);
+            // Console.WriteLine($"The decrypted message is: '{decrypted}'");
+
 
         }
     }
